1a754eaaa8412ad927a471dfe3457d68fabb8c36
Abhishek <abhishekparmar162@gmail.com>
2022-09-30 00:37:42 +0530

    started login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 
+import { useSelector} from 'react-redux';
+import { selectUser } from './features/userSlice';
 import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
+import Login from './components/Login/Login';
+
 
 function App() {
+  const user = useSelector(selectUser);
+    const sideBar = useSelector(state => state.sideBar);
   return (
     <div className="bg-[#f3f2ef] flex flex-col ">
      {/*Header */}
+
+{!user ?  <Login /> : ( 
+     <>
      <Header />
-     {/*SideBar */}
      <div className='flex flex-row m-8'>
       <SideBar />
      <Feed />
      </div>
-     {/*Feed */}
-     {/*app body */}
-     {/*Widgets */}
+     </>
+)}
+{/*SideBar */}
+{/*Feed */}
+{/*app body */}
+{/*Widgets */}
     </div>
+
   );
 }
 
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div>
+<img  src='https://www.citypng.com/public/uploads/preview/hd-linkedin-official-logo-transparent-background-31623962207jz85kqlqot.png' className='text-blue-400 w-200 h-120' alt='linkedin logo'/>
+
+        </div>
+        <div><h1>Make the most of your professional life</h1></div>
+    <div className='bg-white flex flex-row'>
+    <form>
+        <label>Email or phone number</label>
+        <input  type="text" />
+        <label>Password (6 or more characters)</label>
+        <input type="password" />
+    </form>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Login
