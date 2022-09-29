46ebda7f77ec102730d9ddbe802c92dd57ed5705
Abhishek <abhishekparmar162@gmail.com>
2022-09-30 01:00:50 +0530

    created form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Logo from '../../assests/Logo.png'
 const Login = () => {
   return (
-    <div className='grid'>
+    <div className=' bg-gray-200 w-full h-fit'>
         <div className='w-40 ml-48 '>
 <img  src={Logo} alt='linkedin logo'/>
 
         </div>
-        <div><h1>Make the most of your professional life</h1></div>
-    <div className='bg-white flex flex-row'>
-    <form>
+        <h1 className='text-center text-4xl'>Make the most of your professional life</h1>
+    
+    <form className='bg-white flex flex-col text-center w-60 mt-8 h-60 ml-[50%]' >
         <label>Email or phone number</label>
         <input  type="text" />
         <label>Password (6 or more characters)</label>
@@ -18,7 +18,6 @@ const Login = () => {
 
     </div>
 
-    </div>
   )
 }
 
